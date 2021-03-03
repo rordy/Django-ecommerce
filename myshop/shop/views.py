06bd7e46bbe58751298a00db7ddb0902be1766b5
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2021-03-03 23:12:16 +0100

    Added Shop List of products

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -4,13 +4,17 @@ from .models import Category, Product
 # Create your views here.
 
 def product_list(request, category_slug=None):
-    category = None 
+    category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
     if category_slug:
-        category = get_list_or_404(Category, slug=category_slug)
+        category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html',{'category': category, 'categories': categories,'produscts':products})
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
 
 def product_detail(request, id, slug):
     product = get_list_or_404(Product, id=id, slug=slug, available=True)
